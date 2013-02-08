bddee7edf2a39252730add8bdb9e7439d45356ff
proller <proller@github.com>
2013-02-08 22:49:21 +0400

    works with config param

--- src/map.h
+++ src/map.h
@@ -300,6 +300,7 @@ public:
 	virtual void PrintInfo(std::ostream &out);
 
 	void transformLiquids(core::map<v3s16, MapBlock*> & modified_blocks);
+	void transformLiquidsFinite(core::map<v3s16, MapBlock*> & modified_blocks);
 
 	/*
 		Node metadata
